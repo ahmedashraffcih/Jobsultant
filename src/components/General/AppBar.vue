3cfde35660a114429937d3d8692632e44b4207e5
Ahmed Ashraf <ahmedashraffcih@gmail.com>
2020-12-06 04:25:34 +0200

    Adjust_App_Bar

--- src/components/General/AppBar.vue
+++ src/components/General/AppBar.vue
@@ -1,22 +1,29 @@
 <template>
   <!-- flat to remove shadow-->
-  <v-app-bar app color="#03A9F4" dark>
-    <!-- 3 dashes-->
-    <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+  <v-app-bar class="nav" app color="light-blue darken-4" dark flat>
+    <!-- 3 dashes
+    <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>-->
+    <v-avatar size="160px">
+      <img :src="require('@/assets/Logo_on_blue.png')"/>
+    </v-avatar>
 
-    <v-toolbar-title class="title">Jobsultant</v-toolbar-title>
-    <v-btn to="/" class="ml-10" text flat>Home</v-btn>
-    <v-btn to="/about" text flat>Jobs</v-btn>
-    <v-btn to="/user/account_settings" text flat>My Account</v-btn>
-    <v-btn to="/user/My_CV" text flat>My CV</v-btn>
+    <v-toolbar-title class="title"></v-toolbar-title>
+    <v-btn to="/" class="buttoncase ml-10" text flat>Home</v-btn>
+    <v-btn class="buttoncase" to="/about" text flat>Find Jobs</v-btn>
+    <v-btn class="buttoncase" to="/user/account_settings" text flat
+      >My Account</v-btn
+    >
+    <v-btn class="buttoncase" to="/user/My_CV" text flat>My CV</v-btn>
     <v-spacer></v-spacer>
     <v-btn icon>
       <v-icon>mdi-magnify</v-icon>
     </v-btn>
 
-    <v-btn class="mr-4" to="/Details/login" outlined>Log in</v-btn>
-    <v-btn class="mr-4" to="/Details/Register" outlined>Register</v-btn>
-    <v-btn outlined>For Employers</v-btn>
+    <v-btn class="buttoncase mr-4" to="/Details/login" outlined>Log in</v-btn>
+    <v-btn class="buttoncase mr-4" to="/Details/Register" outlined
+      >Register</v-btn
+    >
+    <v-btn class="buttoncase" outlined>For Employers</v-btn>
     <v-menu left bottom>
       <template v-slot:activator="{ on, attrs }">
         <v-btn icon v-bind="attrs" v-on="on">
@@ -25,9 +32,10 @@
       </template>
 
       <v-list>
-        <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-          <v-list-item-title>Option {{ n }}</v-list-item-title>
-        </v-list-item>
+        <v-list-item @click="() => {}">Account Settings</v-list-item>
+        <v-list-item @click="() => {}">For Employers</v-list-item>
+        <v-divider></v-divider>
+        <v-list-item @click="() => {}">Log Out</v-list-item>
       </v-list>
     </v-menu>
   </v-app-bar>
@@ -37,7 +45,12 @@
 <script>
 export default {
   data: () => ({
-    group: null
-  })
+    group: null,
+  }),
 };
 </script>
+<style scoped>
+.buttoncase {
+  text-transform: none;
+}
+</style>
